b5d8c96ce099892a9e194cc39094b86b36f15141
Nahom Melese <nahomjr17@gmail.com>
2024-06-11 11:12:42 -0700

    updated

--- client/src/components/banner.jsx
+++ client/src/components/banner.jsx
@@ -1,14 +1,33 @@
+import bannerImage from '../assets/bannerImage.jpg'
+import  styled from 'styled-components';
+
+const BannerImageContainer = styled.div`
+   background-color:red;
+   height:700px;
+   width:100%;
+   position:fixed; 
+   top:-20px;
+   z-index:-1;
+   padding:100px;
+   padding-top:300px;
+   background-image:url(${bannerImage});
+   background-position: center;
+   background-repeat: no-repeat;
+   background-size: cover;
+
+`;
 
 const Banner = ()=>{
 
 
 return <>
-    <div style={{backgroundColor:'red',height:'600px',width:'100%',position:'fixed', top:'-20px',zIndex:'-1',padding:'100px'}}>
-  iojwdj
+   <BannerImageContainer></BannerImageContainer>
+    <div style={{height:'200px',backgroundColor:'orange',position:'relative',top:'500px',marginBottom:'300px'}}>
+
     </div>
     <div style={{height:'500px',backgroundColor:'blue',position:'relative',top:'500px'}}>
 
-    </div>
+</div>
 </>
 }
 
