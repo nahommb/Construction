a584a4d9166809ea43a6b231c2d6c96639e4615f
Nahom Melese <nahomjr17@gmail.com>
2024-06-25 15:57:01 -0700

    updated

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -6,7 +6,7 @@ import { styled } from "styled-components";
 const Container = styled.div`
 height:400px;
 width:400px;
-background-color:red;
+${'' /* background-color:red; */}
 margin:auto;
 margin-top:20%
 `;
@@ -23,7 +23,39 @@ const dispatch = useDispatch()
  })
     return <>
         <Container>
-            <lebel>Company Name</lebel>
+            
+        <form class="form">
+    <p class="title">Register </p>
+    <p class="message">Signup now and get full access to our app. </p>
+        <div class="flex">
+        <label>
+            <input class="input" type="text" placeholder="" required=""/>
+            <span>Firstname</span>
+        </label>
+
+        <label>
+            <input class="input" type="text" placeholder="" required=""/>
+            <span>Lastname</span>
+        </label>
+    </div>  
+            
+    <label>
+        <input class="input" type="email" placeholder="" required=""/>
+        <span>Email</span>
+    </label> 
+        
+    <label>
+        <input class="input" type="password" placeholder="" required=""/>
+        <span>Password</span>
+    </label>
+    <label>
+        <input class="input" type="password" placeholder="" required=""/>
+        <span>Confirm password</span>
+    </label>
+    <button class="submit">Submit</button>
+    <p class="signin">Already have an acount ? <a href="#">Signin</a> </p>
+</form>
+
         </Container>
     </>
 }
